6f5a839d8a1664076a87edd86d44b7bee2f52a34
Radosław Smoter <radoslaw.sm.tr@gmail.com>
2021-10-23 21:46:22 +0200

    code finished

--- Zajecia_3/wp_r_smoter_3.c
+++ Zajecia_3/wp_r_smoter_3.c
@@ -1,50 +1,64 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "math.h"
 
 /* 
 	* Count a sum with specified parameters
-	* Domain boundaries: first min, then max;
-	* Domain step
-	* Function error
-	* Range begin; for sum
+	* Domain boundaries: first min, then max,
+	* domain step, function error, range begin
 */
-float doSum(float [], float, float, int);
+double doSum(const double [], double, double, int);
+
+/*
+	* Count a mathematical function with specified parameters
+	* Domain value, iteration number specifier
+*/
+double countFunc(double x, int k);
 
 int main(void)
 {
   // Sum consts
-  const float error = .000016; // Function error
-  const float step = .01; // Domain step
-  const float domain[] = { 3., 4. }; // Domain boundaries
+  const double error = .000016; // Function error
+  const double step = .01; // Domain step
+  const double domain[] = { 3., 4. }; // Domain boundaries
 
-  float sum = doSum(domain, step, error, 1);
+  double sum = doSum(domain, step, error, 1);
+	printf("THE_SUM = %.6f\n", sum);
 
   return 0;
 }
 
-float doSum(float domain[], float step, float error, int k) {
+double doSum(
+	const double domain[], double step, double error, int k)
+{
 
-	float sum;
+	double sum = .0;
 
-	for (float i = domain[0]; i < domain[1]; i++) {
+	for (double x = domain[0]; x < domain[1]; x += step) {
 
+		double elem;
+		int l = k;
 
+		do
+		{
+			elem = countFunc(x, l);
+			sum += elem;
+			printf("elem%12.6f\tk%12.6f\n", elem, sum);
+			l++;
+
+		} while (elem > error);
 
 	}
 
-	return;
+	return sum;
 }
 
-float countSin() {
+double countFunc(double x, int k)
+{
 
+	double sinVal = sin(x / pow(2, k));
+	double cosVal = cos(x / pow(2, k));
+	double fractVal = 1 / pow(2, k);
 
-
-	return;
-}
-
-float countCos() {
-
-	
-
-	return;
+	return fractVal * (sinVal + cosVal);
 }
