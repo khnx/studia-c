e3186f98cab27584ea9afe47d0da4f5a9c951dba
Radosław Smoter <radoslaw.sm.tr@gmail.com>
2021-12-19 20:41:17 +0100

    update

---
+++ zad_10/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author Radoslaw Smoter (radoslaw.smoter@student.pk.edu.pl)
+ * @brief Calculate derivatives with Runge-Kutta methods
+ * @version 0.1
+ * @date 2021-12-19
+ * 
+ * @copyright Copyright (c) 2021
+ */
+
+
+int main(void)
+{
+  
+
+
+  return 0;
+}
